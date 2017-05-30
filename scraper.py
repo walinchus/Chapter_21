f9fc63683091fef86df4883f6a724ee00c45b537
walinchus <Lucia@professionalnewsservices.com>
2017-05-30 13:53:32 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,24 +1,26 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+###############################################################################
+# START HERE: Tutorial for scraping pages behind form, using the
+# very powerful Mechanize library. Documentation is here: 
+# http://wwwsearch.sourceforge.net/mechanize/
+###############################################################################
+import mechanize 
+import lxml.html
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+lotterygrantsurl = "http://www.lottery.culture.gov.uk/AdvancedSearch.aspx"
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+br = mechanize.Browser()
+response = br.open(lotterygrantsurl)
+
+print "All forms:", [ form.name  for form in br.forms() ]
+
+br.select_form(name="aspnetForm")
+print br.form
+
+br["ctl00$phMainContent$dropDownAwardDate"] = ["Between"]
+br["ctl00$phMainContent$txtGrantDateFrom"] = "01/01/2004"
+br["ctl00$phMainContent$txtGrantDateTo"]  = "20/01/2004"
+
+
+response = br.submit()
+print response.read()
+
